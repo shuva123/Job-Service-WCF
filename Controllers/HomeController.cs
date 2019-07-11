6f97f531a2b251aeed846fd06d2f446b4c801393
shuva123 <33460198+shuva123@users.noreply.github.com>
2019-07-11 17:03:24 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobServiceClient2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            JobServiceReference.JobsServiceClient jobsServiceClient = new JobServiceReference.JobsServiceClient();
+             
+            return View(jobsServiceClient.OpeningJobs());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JobServiceClient2.MvcApplication" Language="C#" %>
